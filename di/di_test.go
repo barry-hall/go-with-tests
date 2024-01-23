2e2ed0d4835707d7a63f8ef9e8ced91cc575352b
Barry Hall <io.barryhall@gmail.com>
2024-01-23 09:09:52 +0000

    feat: di

--- di/di_test.go
+++ di/di_test.go
@@ -1,9 +1,19 @@
-package di
+package main
 
 import (
+	"bytes"
 	"testing"
 )
 
 func TestGreet(t *testing.T) {
+	buffer := bytes.Buffer{}
 
+	Greet(&buffer, "River")
+
+	got := buffer.String()
+	want := "Hello, River"
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
 }
