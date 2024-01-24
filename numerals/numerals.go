8ad78089294462f35b145be0b01f7a6aa15f85a1
Barry Hall <io.barryhall@gmail.com>
2024-01-24 11:51:10 +0000

    feat: adds table for numeral values and new convert logic

--- numerals/numerals.go
+++ numerals/numerals.go
@@ -1,8 +1,30 @@
 package numerals
 
+import "strings"
+
+type RomanNumeral struct {
+	Value int
+
+	Symbol string
+}
+
+var allRomanNumerals = []RomanNumeral{
+	{10, "X"},
+	{9, "IX"},
+	{5, "V"},
+	{4, "IV"},
+	{1, "I"},
+}
+
 func ConverToRoman(arabic int) string {
-	if arabic == 2 {
-		return "II"
+	var result strings.Builder
+
+	for _, numeral := range allRomanNumerals {
+		for arabic >= numeral.Value {
+			result.WriteString(numeral.Symbol)
+			arabic -= numeral.Value
+		}
 	}
-	return "I"
+
+	return result.String()
 }
--- numerals/numerals_test.go
+++ numerals/numerals_test.go
@@ -10,6 +10,15 @@ func TestRomanNumers(t *testing.T) {
 	}{
 		{"1 gets converted to I", 1, "I"},
 		{"2 gets converted to II", 2, "II"},
+		{"3 gets converted to III", 3, "III"},
+		{"4 gets converted to IV", 4, "IV"},
+		{"5 gets converted to V", 5, "V"},
+		{"9 gets converted to IX", 9, "IX"},
+		{"10 gets converted to X", 10, "X"},
+		{"14 gets converted to XIV", 14, "XIV"},
+		{"18 gets converted to XVIII", 18, "XVIII"},
+		{"20 gets converted to XX", 20, "XX"},
+		{"39 gets converted to XXXIX", 39, "XXXIX"},
 	}
 
 	for _, test := range cases {
