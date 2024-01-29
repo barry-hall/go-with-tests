d9c960cd747ac703748e719f7a864ff8fd278c03
Barry Hall <io.barryhall@gmail.com>
2024-01-29 18:31:56 +0000

    feat: adds more functionality to templating

---
+++ templating/templating.go
@@ -0,0 +1,12 @@
+package templating
+
+import (
+	"fmt"
+	"go-with-tests/files"
+	"io"
+)
+
+func Render(w io.Writer, p files.Post) error {
+	fmt.Println("todo...")
+	return nil
+}
--- templating/templating_test.go
+++ templating/templating_test.go
@@ -1,5 +1,35 @@
-package templating
+package templating_test
 
-func main() {
+import (
+	"bytes"
+	"go-with-tests/files"
+	"go-with-tests/templating"
+	"testing"
+)
 
+func TestRender(t *testing.T) {
+	var (
+		aPost = files.Post{
+			Title:       "Hello, World!",
+			Body:        "This is a post",
+			Description: "Lorem ipsum dolor sit amet",
+			Tags:        []string{"go", "tdd"},
+		}
+	)
+
+	t.Run("it converts a single post into HTML", func(t *testing.T) {
+		buf := bytes.Buffer{}
+		err := templating.Render(&buf, aPost)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		got := buf.String()
+		want := `<h1>Hello, World!</h1>`
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
 }
