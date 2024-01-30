428e414c8cb64a760bc196492aa3566c19f5c29b
Barry Hall <io.barryhall@gmail.com>
2024-01-30 10:02:12 +0000

    feat: adds templating and assert library for testing snapshots

--- templating/templating.go
+++ templating/templating.go
@@ -1,12 +1,36 @@
 package templating
 
 import (
-	"fmt"
+	"embed"
 	"go-with-tests/files"
+	"html/template"
 	"io"
 )
 
-func Render(w io.Writer, p files.Post) error {
-	fmt.Println("todo...")
+var (
+	//go:embed "templates/*"
+	postTemplates embed.FS
+)
+
+type PostRenderer struct {
+	templ *template.Template
+}
+
+func NewPostRendered() (*PostRenderer, error) {
+	templ, err := template.ParseFS(postTemplates, "templates/*.gohtml")
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &PostRenderer{templ}, nil
+}
+
+func (r *PostRenderer) Render(w io.Writer, p files.Post) error {
+	if err := r.templ.ExecuteTemplate(w, "blog.gohtml", p); err != nil {
+		return err
+	}
+
 	return nil
+
 }
--- templating/templating_test.go
+++ templating/templating_test.go
@@ -4,7 +4,10 @@ import (
 	"bytes"
 	"go-with-tests/files"
 	"go-with-tests/templating"
+	"io"
 	"testing"
+
+	approvals "github.com/approvals/go-approval-tests"
 )
 
 func TestRender(t *testing.T) {
@@ -17,19 +20,41 @@ func TestRender(t *testing.T) {
 		}
 	)
 
+	postRenderer, err := templating.NewPostRendered()
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
 	t.Run("it converts a single post into HTML", func(t *testing.T) {
 		buf := bytes.Buffer{}
-		err := templating.Render(&buf, aPost)
 
-		if err != nil {
-			t.Fatal(err)
+		if err := postRenderer.Render(&buf, aPost); err != nil {
+			t.Fatalf("Render() err = %s", err)
 		}
 
-		got := buf.String()
-		want := `<h1>Hello, World!</h1>`
-
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		approvals.VerifyString(t, buf.String())
 	})
+}
+
+func BenchmarkRender(b *testing.B) {
+	var (
+		aPost = files.Post{
+			Title:       "Hello, World!",
+			Body:        "This is a post",
+			Description: "Lorem ipsum dolor sit amet",
+			Tags:        []string{"go", "tdd"},
+		}
+	)
+
+	postRenderer, err := templating.NewPostRendered()
+
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		postRenderer.Render(io.Discard, aPost)
+	}
 }
