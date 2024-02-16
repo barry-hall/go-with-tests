0e3c050a851878101f1d64780ff02d4cfa676a6e
Barry Hall <io.barryhall@gmail.com>
2024-02-16 11:00:27 +0000

    feat: handle empty file

--- httpserver/server/file_system_store.go
+++ httpserver/server/file_system_store.go
@@ -13,7 +13,19 @@ type FileSystemPlayerStore struct {
 
 func NewFileSystemPlayerStore(file *os.File) (*FileSystemPlayerStore, error) {
 	file.Seek(0, 0)
+
+	err := initialisePlayerDbFile(file)
+
+	if err != nil {
+		return nil, fmt.Errorf("problem initialising player db file, %v", err)
+	}
+
+	if err != nil {
+		return nil, fmt.Errorf("problem initialising player db file, %v", err)
+
+	}
 	league, err := NewLeague(file)
+
 	if err != nil {
 		return nil, fmt.Errorf("problem loading player store from file %s, %v", file.Name(), err)
 	}
@@ -22,6 +34,23 @@ func NewFileSystemPlayerStore(file *os.File) (*FileSystemPlayerStore, error) {
 		Database: json.NewEncoder(&Tape{file}),
 		League:   league,
 	}, nil
+}
+
+func initialisePlayerDbFile(file *os.File) error {
+	file.Seek(0, 0)
+
+	info, err := file.Stat()
+
+	if err != nil {
+		return fmt.Errorf("problem getting file info from file %s, %v", file.Name(), err)
+	}
+
+	if info.Size() == 0 {
+		file.Write([]byte("[]"))
+		file.Seek(0, 0)
+	}
+
+	return nil
 }
 
 func (f *FileSystemPlayerStore) GetLeague() League {
